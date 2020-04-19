495c7d541e0f7a6a8b76921b2baf9c2dfc536ac3
irtiza-S <irtizasafdar@hotmail.co.uk>
2020-04-19 03:50:20 +0100

    added new functions

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//calculator
+//calculator - js code
+
+//1.
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b; 
+}
+
+//2. 
+function operate(op, x, y){
+    if(op === '+'){
+        return add(x, y);
+    }
+
+    else if (op === '-'){
+        return subtract(x, y);
+    }
+
+    else if (op === '*'){
+        return multiply(x, y);
+    }
+
+    else{
+        return divide(x,y);
+    }
+}
